13be81955318406b2ef3f29f8fe5d7abdcd507b6
RandyCoffman <randy.coffman@wvjc.edu>
2018-03-20 11:08:35 -0400

    changed the app to use sessions instead of pushing data in the url

--- app.rb
+++ app.rb
@@ -1,89 +1,62 @@
 require "sinatra"
+enable :sessions
 
 get "/" do
 	erb :first_page
 end
 
 post "/name" do
-	name = params[:name]
-	redirect "/name_redirect?name=" + name
+	session[:name] = params[:name]
+	redirect "/name_redirect"
 end 
 
 get "/name_redirect" do 
-	f_name = params[:name]
-	erb :second_page, locals:{f_name: f_name}
+	erb :second_page
 end
 
 post "/age" do
-	f_name = params[:f_name]
-	age = params[:age]
-	redirect "/favorite_food?f_name=" + f_name + "&age=" + age
+	session[:age] = params[:age]
+	redirect "/favorite_food"
 end
 
 get "/favorite_food" do
-	f_name = params[:f_name]
-	age = params[:age]
-	erb :third_page, locals:{f_name: f_name, age: age}
+	erb :third_page
 end
 
 post "/food_drink" do
-	f_name = params[:f_name]
-	age = params[:age]
-	food = params[:food]
-	drink = params[:drink]
-	redirect "/favorite_numbers?f_name=" + f_name + "&age=" + age + "&food=" + food + "&drink=" + drink
+	session[:food] = params[:food]
+	session[:drink] = params[:drink]
+	redirect "/favorite_numbers"
 end
 
 get "/favorite_numbers" do 
-	f_name = params[:f_name]
-	age = params[:age]
-	food = params[:food]
-	drink = params[:drink]
-	erb :fourth_page, locals:{f_name: f_name, age: age, food: food, drink: drink}
+	erb :fourth_page
 end
 
 post "/numbers" do
-	f_name = params[:f_name]
-	age = params[:age]
-	food = params[:food]
-	drink = params[:drink]
-	fav_num_one = params[:fav_num_one]
-	fav_num_two = params[:fav_num_two]
-	fav_num_three = params[:fav_num_three]
-	redirect "/favorite_color?f_name=" + f_name + "&age=" + age + "&food=" + food + "&drink=" + drink + "&fav_num_one=" + fav_num_one + "&fav_num_two=" + fav_num_two + "&fav_num_three=" + fav_num_three
+	session[:fav_num_one] = params[:fav_num_one]
+	session[:fav_num_two] = params[:fav_num_two]
+	session[:fav_num_three] = params[:fav_num_three]
+	redirect "/favorite_color"
 end
 
 get "/favorite_color" do 
-	f_name = params[:f_name]
-	age = params[:age]
-	food = params[:food]
-	drink = params[:drink]
-	fav_num_one = params[:fav_num_one]
-	fav_num_two = params[:fav_num_two]
-	fav_num_three = params[:fav_num_three]
-	erb :fifth_page, locals:{f_name: f_name, age: age, food: food, drink: drink, fav_num_one: fav_num_one, fav_num_two: fav_num_two, fav_num_three: fav_num_three}
+	erb :fifth_page
 end
 
 post "/colors" do
-	f_name = params[:f_name]
-	age = params[:age]
-	food = params[:food]
-	drink = params[:drink]
-	fav_num_one = params[:fav_num_one]
-	fav_num_two = params[:fav_num_two]
-	fav_num_three = params[:fav_num_three]
-	fav_color = params[:fav_color]
-	redirect "/results?f_name=" + f_name + "&age=" + age + "&food=" + food + "&drink=" + drink + "&fav_num_one=" + fav_num_one + "&fav_num_two=" + fav_num_two + "&fav_num_three=" + fav_num_three + "&fav_color=" + fav_color
+	session[:fav_color] = params[:fav_color]
+	redirect "/results" 
 end
 
 get "/results" do
-	f_name = params[:f_name]
-	age = params[:age]
-	food = params[:food]
-	drink = params[:drink]
-	fav_num_one = params[:fav_num_one]
-	fav_num_two = params[:fav_num_two]
-	fav_num_three = params[:fav_num_three]
-	fav_color = params[:fav_color]
-	erb :sixth_page, locals:{f_name: f_name, age: age, food: food, drink: drink, fav_num_one: fav_num_one, fav_num_two: fav_num_two, fav_num_three: fav_num_three, fav_color: fav_color}
+	name = session[:name]
+	age = session[:age]
+	food = session[:food]
+	drink = session[:drink]
+	fav_num_one = session[:fav_num_one]
+	fav_num_two = session[:fav_num_two]
+	fav_num_three = session[:fav_num_three]
+	fav_color = session[:fav_color]
+	erb :sixth_page, locals:{name: name, age: age, food: food, drink: drink, fav_num_one: fav_num_one, fav_num_two: fav_num_two, fav_num_three: fav_num_three, fav_color: fav_color}
 end
